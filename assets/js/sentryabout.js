4701c33c5f2a596c186321083826097804b27366
bin4xin <1904829268@qq.com>
2022-01-22 17:51:44 +0800

    [2022/01/22/17:51:13]<TODO::footer页面点击内容收起>commit by bin4xin.
    Signed-off-by: bin4xin <1904829268@qq.com>

--- assets/js/sentryabout.js
+++ assets/js/sentryabout.js
@@ -38,4 +38,77 @@ $(document).scroll(function(){
 		//console.log("show article index")
 	}
 });
+
+
+
+/*
+* ----------------------------------------------------------------------------------------------------------
+* TODO: 按钮点击展开ul事件测试
+* ----------------------------------------------------------------------------------------------------------
+ */
+// ----------------------------------------------------------------------------------------------------------
+// 2.	按钮点击展开ul事件测试2
+
+
+
+// ----------------------------------------------------------------------------------------------------------
+// 1.	按钮点击展开ul事件测试1 多个按钮点击会同时展开关闭
+// $(function(){
+// 	$(".fold-button").click(function(){
+// 		$(".fold-box").slideToggle("slow");
+// 		$(".open").toggle();
+// 		$(".close").toggle();
+// 	});
+// });
+// ----------------------------------------------------------------------------------------------------------
+
+
+// $(function () {
+// 	$(".footer-row button").click(function(){
+// 		var $this = $(this);
+// 		if($this.hasClass("rotate-90")) {
+// 			$this.removeClass("rotate-90").addClass("rotate-270").val("收起");
+// 		} else {
+// 			$this.removeClass("rotate-270").addClass("rotate-90").val("展开");
+// 		}
+// 	});
+// });
+
+// $(document).ready(function(){
+//
+// 	var preResult = $('.ul dn').height();
+// 	var pre = $('.ul dn-ns').height();
+// 	$(".dn-m").click(function() {
+// 		if ($this.hasClass("dn dn-ns dn-m")) {
+// 			$this.removeClass("dn dn-ns dn-m").addClass("db db-ns db-m").val("收起");
+// 		} else {
+// 			$this.removeClass("db db-ns db-m").addClass("dn dn-ns dn-m").val("展开");
+// 		}
+// 	});
+// });
+
+/*
+* ----------------------------------------------------------------------------------------------------------
+* ----------------------------------------------------------------------------------------------------------
+ */
+
+// $(function() {
+// 	$(".footer-row button").click(function(){
+// 		var $this = $(this);
+// 		if($this.hasClass("rotate-90")) {
+// 			$this.removeClass("rotate-90").addClass("rotate-270").val("收起");
+// 		} else {
+// 			$this.removeClass("rotate-270").addClass("rotate-90").val("展开");
+// 		}
+// 		$(".div ul").click(function(){
+// 			// var $this = $(this);
+// 			if($this.hasClass("dn dn-ns dn-m")) {
+// 				$this.removeClass("dn dn-ns dn-m").addClass("db db-ns db-m").val("收起");
+// 			} else {
+// 				$this.removeClass("db db-ns db-m").addClass("dn dn-ns dn-m").val("展开");
+// 			}
+// 		});
+// 	});
+// });
+
 // vim: ts=2 sts=2 sw=2 noexpandtab
