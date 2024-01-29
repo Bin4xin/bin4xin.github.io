66ee6601349911cb3b3d33ae01869d2f380d5cc8
bin4xin <57884471+Bin4xin@users.noreply.github.com>
2024-01-29 11:02:20 +0800

    commit `snippets.sh` script to check website version v1.0.2
    Signed-off-by: bin4xin <57884471+Bin4xin@users.noreply.github.com>

--- scripts/snippets.sh
+++ scripts/snippets.sh
@@ -3,7 +3,7 @@
 version_check(){
   # check version of website.
   web_version=$(grep web_version _config.yml| awk -F':' '{print $2}'| tr -d ' ')
-  # use awk write $web_version into README.MD
+  # use awk write $web_version into README.md
   echo "[LOG]: config file version: '$web_version'"
   old_web_version=$(grep "Power by Bin4xin" README.md|awk -F' ' '{print $3}')
   echo "[LOG]: README version: '$old_web_version'"
@@ -11,7 +11,7 @@ version_check(){
   if [ "$old_web_version" != "$web_version" ];then
     echo "[LOG]: diff version detected."
     tmpfileContent=$(sed "s/$old_web_version/$web_version/g" README.md)
-    echo "$tmpfileContent" > README.MD
+    echo "$tmpfileContent" > README.md
     echo "[LOG]: Success write into new web version."
   else
     echo "[LOG]: No diff in version, \$version_check passing."
